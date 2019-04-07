acb59caeb25153d30bca1b182538a3eb8b3c3920
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-04-07 22:18:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def greeting(name)
+def introduction(name)
   puts "Hello, my name is #{name}."
 end
 
-def welcome(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
